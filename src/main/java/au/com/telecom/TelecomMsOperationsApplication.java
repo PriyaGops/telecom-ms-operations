b0bd848cf8020a3e8b6bcc9043fadd0e44d8833a
priyankasathurappan <priyanka.gopalan25@gmail.com>
2023-02-07 12:10:12 +1100

    initial commit

---
+++ src/main/java/au/com/telecom/TelecomMsOperationsApplication.java
@@ -0,0 +1,13 @@
+package au.com.telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomMsOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomMsOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/telecom/TelecomMsOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.telecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomMsOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
